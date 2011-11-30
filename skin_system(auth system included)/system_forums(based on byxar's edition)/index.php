0cb4408a1f73c3089777143e37a69db52f0e700a
byxar <multimeter42@gmail.com>
2011-11-30 15:08:50 +0200

    commit

--- skin_system(auth system included)/system_forums(based on byxar's edition)/index.php
+++ skin_system(auth system included)/system_forums(based on byxar's edition)/index.php
@@ -73,29 +73,24 @@ require 'config.php';
 				$_POST['rememberMe'] = (int)$_POST['rememberMe'];
 				$postPass = $_POST['password'];
 
-				switch ($crypt)
+				if ($crypt == 'hash_md5' || $crypt == 'hash_authme' || $crypt == 'hash_xauth' || $crypt == 'hash_cauth' || $crypt == 'hash_joomla' || $crypt == 'hash_wordpress')
 				{
-					case 'hash_md5':
-					case 'hash_authme':
-					case 'hash_xauth':
-					case 'hash_cauth':
-					case 'hash_joomla':
-					case 'hash_wordpress':
-						$row = mysql_fetch_assoc(mysql_query("SELECT $db_columnId,$db_columnUser,$db_columnPass FROM $db_table WHERE $db_columnUser='{$_POST['username']}'"));
-						$realPass = $row[$db_columnPass];
-					break;
+					$row = mysql_fetch_assoc(mysql_query("SELECT $db_columnId,$db_columnUser,$db_columnPass FROM $db_table WHERE $db_columnUser='{$_POST['username']}'"));
+					$realPass = $row[$db_columnPass];
+				}
 
-					case 'hash_ipb':
-						$row = mysql_fetch_assoc(mysql_query("SELECT $db_columnId,$db_columnUser,$db_columnPass,$db_columnSalt FROM $db_table WHERE $db_columnUser='{$_POST['username']}'"));
-						$realPass = $row[$db_columnPass];
-						$salt = $row[$db_columnSalt];
-					break;
+				if ($crypt == 'hash_ipb')
+				{
+					$row = mysql_fetch_assoc(mysql_query("SELECT $db_columnId,$db_columnUser,$db_columnPass,$db_columnSalt FROM $db_table WHERE $db_columnUser='{$_POST['username']}'"));
+					$realPass = $row[$db_columnPass];
+					$salt = $row[$db_columnSalt];
+				}
 					
-					case 'hash_xenforo':
-						$row = mysql_fetch_assoc(mysql_query("SELECT $db_table.$db_columnId,$db_table.$db_columnUser,$db_tableOther.$db_columnId,$db_tableOther.$db_columnPass FROM $db_table, $db_tableOther WHERE $db_table.$db_columnId = $db_tableOther.$db_columnId AND $db_table.$db_columnUser='{$_POST['username']}'"));
-						$realPass = substr($row[$db_columnPass],22,64);
-						$salt = substr($row[$db_columnPass],105,64);
-					break;
+				if ($crypt == 'hash_xenforo')
+				{
+					$row = mysql_fetch_assoc(mysql_query("SELECT $db_table.$db_columnId,$db_table.$db_columnUser,$db_tableOther.$db_columnId,$db_tableOther.$db_columnPass FROM $db_table, $db_tableOther WHERE $db_table.$db_columnId = $db_tableOther.$db_columnId AND $db_table.$db_columnUser='{$_POST['username']}'"));
+					$realPass = substr($row[$db_columnPass],22,64);
+					$salt = substr($row[$db_columnPass],105,64);
 				}
 
 					$checkPass = $crypt();
